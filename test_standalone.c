865e77e74d5a99afd9270e07764205a315d3df6f
Sergey Alirzaev <zl29ah@gmail.com>
2019-08-15 19:09:56 +0300

    add sigaction meddling to test_standalone

--- test_standalone.c
+++ test_standalone.c
@@ -1,6 +1,20 @@
+#include <signal.h>
+#include <stdio.h>
 #include <stdlib.h>
+
+static void handler(int signum, siginfo_t *siginfo, void *wat)
+{
+	fputs("test failed!\n", stderr);
+}
 
 int main()
 {
+	sigset_t set;
+	sigemptyset(&set);
+	struct sigaction act;
+	act.sa_sigaction = handler;
+	act.sa_mask = set;
+	act.sa_flags = SA_SIGINFO;
+	sigaction(SIGABRT, &act, 0);
 	abort();
 }
